6a833f94e6676b4a1c17514552f5ecedcf4d4823
TonyAyala <antonioayalam2001@gmail.com>
2023-02-17 16:18:37 -0600

    Instrucciones y pruebas primer ejercicio

--- hasUniqueCharacters.js
+++ hasUniqueCharacters.js
@@ -1,3 +1,13 @@
+
+// De los siguientes problemas, selecciona uno y resuélvelo usando JS (puntos extra por usar pruebas
+//     TDD)
+//     1.- Implementa una función que determine si una cadena de texto dada esta formada por caracteres
+//     únicos, considera mayúsculas, minúsculas y caracteres especiales
+//     (3 pts)
+//     //Por Ejemplo
+//     hasUniqueCharacters("String") // true
+//     hasUniqueCharacters("Some String") // false
+
 const hasUniqueCharacters = (string = '') => {
     const dictionary = {};
     const stringWithoutSpaces = string.replace(/([^a-zA-Z])\w/g, "")
@@ -17,8 +27,5 @@ const hasUniqueCharacters = (string = '') => {
 
 }
 
-if (hasUniqueCharacters("Unique")) {
-    console.log("The string has unique charachters")
-} else {
-    console.log('Some characters are repeated')
-}
+console.log(hasUniqueCharacters("String"));
+console.log(hasUniqueCharacters("String String"));
